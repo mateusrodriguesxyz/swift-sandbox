6cbb02f15ae674bb8fde83b56b465bf8c5596986
Mateus Rodrigues <mateus.fr26@gmail.com>
2025-02-10 09:05:13 -0300

    Update

--- Package.swift
+++ Package.swift
@@ -4,16 +4,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftSandbox",
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-    ],
     targets: [
         .executableTarget(
             name: "SwiftSandbox",
-            dependencies: []),
-        .testTarget(
-            name: "SwiftSandboxTests",
-            dependencies: ["SwiftSandbox"]),
+            dependencies: []
+        )
     ]
 )
--- Tests/SwiftSandboxTests/SwiftSandboxTests.swift
+++
@@ -1,8 +0,0 @@
-import Testing
-
-@testable import SwiftSandbox
-
-@Test func example() async throws {
-    #expect(3 == 5)
-    // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-}
